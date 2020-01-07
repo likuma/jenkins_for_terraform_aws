ae931046be221ca7630a6b041094eb5eea0c6b2a
oracle <oracle@akademia.(none)>
2020-01-07 21:48:14 +0100

    final

--- autoscaling/autoschedule.tf
+++ autoscaling/autoschedule.tf
@@ -6,7 +6,8 @@ resource "aws_autoscaling_schedule" "ass_dec_for_http_server" {
   #start_time = "2020-01-04T16:15:00Z"
   #end_time = "2020-01-04T15:55:00Z"
   recurrence = "00,10,20,30,40,50 16-20 4-20 1-3 4-6"
-  autoscaling_group_name = "asg_for_http_server"
+  autoscaling_group_name = "${aws_autoscaling_group.asg_for_http_server.name}" #deprecated syntax
+
 
  }
 
@@ -18,5 +19,5 @@ resource "aws_autoscaling_schedule" "ass_inc_for_http_server" {
   #start_time = "2020-01-04T15:55:00Z"
   #end_time = "2020-01-04T16:25:00Z"
   recurrence = "05,15,25,35,45,55 16-20 4-20 1-3 4-6"
-  autoscaling_group_name = "asg_for_http_server"
+  autoscaling_group_name = aws_autoscaling_group.asg_for_http_server.name #new sytnax
  }
